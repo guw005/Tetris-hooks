573fae516db4d2636b30c9a0d28594d6fd229047
Guanyao Wang <guw005@eng.ucsd.edu>
2021-03-11 22:54:48 -0800

    first commit

---
+++ src/components/stage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyledStage } from './styles/styledstage';
+import Cell from './cell';
+
+const Stage = ({ stage }) => (
+    <StyledStage width={stage[0].length} height={stage.length}>
+        {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
+    </StyledStage>
+);
+
+export default Stage;
